fb723c1b02487c0d6476e8ab874aab80cf345832
JaeHee-K <xcv20123@naver.com>
2021-06-26 13:40:21 +0900

    2021-06-26 13:39 String Test 완료 Set Test 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,40 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.InstanceOfAssertFactories.predicate;
 
 public class StringTest {
     @Test
+    @DisplayName("요구사항 테스트")
     void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+        /**
+         * 요구사항 1
+         */
+//        String[] act = "1,2".split(",");
+//        assertThat(act).contains("1", "2");
+
+//        String[] act = "1".split(",");
+//        assertThat(act).containsExactly("1");
+
+        /**
+         * 요구사항 2
+         */
+//        String act = "(1,2)".substring(0,4);
+//        assertThat(act).contains("1,2");
+
+        /**
+         * 요구사항 3
+         */
+//        String act = "abc";
+//        int index = 1;
+//        assertThat(act.charAt(index)).isEqualTo('b');
+
+//        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+//            act.charAt(index);
+//        });
+
     }
 }
