6c5cc812b6f46227a1f11735b78406a5da0506b7
jaeheekim <xcv20123@naver.com>
2021-06-27 14:52:11 +0900

    숫자야구게임 구현 - 2차 수정  - 단위 테스트 미구현  - else 예약어 사용 (수정사항)  - depth 조정

--- src/test/java/study/BaseballGame.java
+++ src/test/java/study/BaseballGame.java
@@ -6,12 +6,71 @@ import java.util.Scanner;
 
 public class BaseballGame {
     static BaseballGame baseballGame = new BaseballGame();
+    static BaseballGame.InputView inputView = baseballGame.new InputView();
+    static BaseballGame.ResultView resultView = baseballGame.new ResultView();
     Scanner scanner = new Scanner(System.in);
-    static int[] answer;
+    static int[] answer = baseballGame.randNumber();
+    static int strike, ball;
+
+    class InputView{
+        public int[] numberInput(){
+            System.out.print("숫자를 입력해 주세요 : ");
+            int value = scanner.nextInt();
+            int[] values = inputConversion(value);
+            int[] reverse_value = reverseNumber(values);
+            return reverse_value;
+        }
+        public int[] inputConversion(int value){
+            int[] values = new int[3];
+            for(int i=0;i<3;i++){
+                values[i] = (value % (int)(Math.pow(10, (i + 1)))) / (int)(Math.pow(10, i));
+            }
+            return values;
+        }
+        public int[] reverseNumber(int[] values){
+            int[] reverse_values = new int[3];
+            for(int i=2,j=0;i>-1;i--,j++){
+                reverse_values[j] = values[i];
+            }
+            return reverse_values;
+        }
+    }
+
+    class ResultView{
+        public void gameResult(){
+            // 테스트 코드(삭제예정)
+            System.out.println("answer : " + Arrays.toString(answer));
+            //
+            System.out.println("strike : " + strike);
+            System.out.println("ball : " + ball);
+            if(strike == 3){
+                resultView.gameFinish();
+            }
+        }
+        public void gameFinish(){
+            System.out.println("3개의 숫자를 모두맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            while(true) {
+                finishCheck();
+            }
+        }
+        public void finishCheck(){
+            int game_select = scanner.nextInt();
+            if (game_select == 1) {
+                answer = baseballGame.randNumber();
+                baseballGame.gameStart();
+            } else if (game_select == 2) {
+                System.out.println("게임이 종료되었습니다.");
+                System.exit(0);
+            } else {
+                System.out.println("1 또는 2를 입력하세요.");
+            }
+        }
+    }
 
     public int[] randNumber(){
         Random rand = new Random();
-        int[] answer = new int[3];
+        answer = new int[3];
 
         for(int i=0;i<3;i++){
             answer[i] = rand.nextInt(9) + 1;
@@ -24,65 +83,33 @@ public class BaseballGame {
         return answer;
     }
 
-    public void gameStart(int[] answer){
+    public void gameStart(){
         while(true) {
-            int strike = 0;
-            int ball = 0;
-
-            System.out.print("숫자를 입력해 주세요 : ");
-            int value = scanner.nextInt();
-            int[] values = new int[3];
-            int[] reverse_values = new int[3];
-            for(int i=0;i<3;i++){
-                values[i] = (value % (int)(Math.pow(10, (i + 1)))) / (int)(Math.pow(10, i));
-            }
-            for(int i=2,j=0;i>-1;i--,j++){
-                reverse_values[j] = values[i];
-            }
-
-            for(int i=0;i<3;i++){
-                if(answer[i] == reverse_values[i]){
-                    strike++;
-                    continue;
-                }
-                for(int j=0;j<3;j++){
-                    if(answer[i] == reverse_values[j]){
-                        ball++;
-                        continue;
-                    }
-                }
-            }
-
-            System.out.println("answer : " + Arrays.toString(answer));
-            System.out.println("input : " + Arrays.toString(reverse_values));
-            System.out.println("strike : " + strike);
-            System.out.println("ball : " + ball);
-
-            if(strike == 3){
-                gameFinish();
-            }
+            int[] inputValue = inputView.numberInput();
+            strikeCheck(inputValue);
+            resultView.gameResult();
         }
     }
 
-    public void gameFinish(){
-        System.out.println("3개의 숫자를 모두맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        while(true) {
-            int game_select = scanner.nextInt();
-            if (game_select == 1) {
-                answer = baseballGame.randNumber();
-                baseballGame.gameStart(answer);
-            } else if (game_select == 2) {
-                System.out.println("게임이 종료되었습니다.");
-                System.exit(0);
-            } else {
-                System.out.println("1 또는 2를 입력하세요.");
+    public void strikeCheck(int[] inputValue){
+        strike = 0;
+        ball = 0;
+
+        for(int i=0;i<3;i++){
+            if(answer[i] == inputValue[i]){
+                strike++;
+                continue;
+            }
+            for(int j=0;j<3;j++){
+                if(answer[i] == inputValue[j]){
+                    ball++;
+                    continue;
+                }
             }
         }
     }
 
     public static void main(String[] args) {
-        answer = baseballGame.randNumber();
-        baseballGame.gameStart(answer);
+        baseballGame.gameStart();
     }
 }
